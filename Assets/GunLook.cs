eb3bf3a10dfc9df33b36f459978a62226e7c1fd1
Tyler Lindberg <tlindberg@ucla.edu>
2016-01-29 23:17:08 -0800

    gunlook added'

---
+++ Assets/GunLook.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunLook : MonoBehaviour {
+
+	private Camera cam;
+
+	void Start () {
+		cam = Camera.main;
+	}
+
+	void Update () {
+		RaycastHit hit;
+		Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast (ray, out hit)) {
+			this.transform.LookAt(hit.point);
+		}
+	}
+}
