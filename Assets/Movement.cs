2066b3abcddc587751bdf39f3c467405913ffaf2
oozgur <oozgur217@gmail.com>
2016-01-29 23:50:24 -0800

    Added shooting

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 
 public class Movement : MonoBehaviour {
 
-	int speed = 10;
+	public int speed = 10;
 
 	public List<GameObject> rails; 
 	public GameObject railObj;
 
+	public GameObject reticle;
+
 	void Start () {
-	
+		Cursor.visible = false;
 	}
 	
 	void Update () {
@@ -21,5 +23,18 @@ public class Movement : MonoBehaviour {
 			Destroy (rails [0]);
 			rails.RemoveAt (0);
 		}
+
+		if (Input.GetKey (KeyCode.R)) {
+			Application.LoadLevel (Application.loadedLevel);
+		}
+
+		var mousePos = Input.mousePosition;
+		mousePos.x -= Screen.width/2;
+		mousePos.y -= Screen.height/2;
+		mousePos.x /= 300;
+		mousePos.y /= 300;
+		mousePos.x -= 9.5f;
+		mousePos.y += 3.875f;
+		reticle.transform.localPosition = new Vector3 (mousePos.x, mousePos.y, transform.position.z + 1.2f);
 	}
 }
---
+++ Assets/Shoot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bullet;
+	public GameObject gun;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject newBullet = Instantiate (bullet, transform.position, Quaternion.identity) as GameObject;
+			newBullet.GetComponent<Rigidbody> ().velocity = gun.transform.forward * 50;
+		}
+	}
+}
